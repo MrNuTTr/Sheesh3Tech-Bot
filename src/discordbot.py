4b513d84042479c84438f2d9dce04ed5f5428528
Eli <eliretro1@gmail.com>
2022-03-26 22:25:15 -0400

    Added a message dictionary

--- src/discordbot.py
+++ src/discordbot.py
@@ -1,12 +1,11 @@
 import discord
 import json
+import messages
 
 # Load the file containing needed tokens
 token_file = open("tokens.json")
-
 # Parse the json into a python object
 tokens = json.load(token_file)
-
 # Close the file
 token_file.close()
 
@@ -18,51 +17,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # If the bot sent the message, then ignore
     if message.author == client.user:
         return
 
-    if message.content == "hlp Hi" or message.content == "hlp hi":
-        await message.channel.send("Hello! Thank you for contacting C3 Tech! A tech will be able to assist you shortly!")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "hlp remote" or message.content == "hlp Remote":
-        await message.channel.send("Hi! Please go to help.myc3.tech and a tech will give you were code when you are at the screen that says 'please join with a code!'")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "hlp pam" or message.content == "hlp Pam":
-        await message.channel.send("PIss off")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "hlp ur mum" or message.content == "hlp your mom" or message.content == "ur mom":
-        await message.channel.send("My Mom? I think you mean your mom cause I did her last night and im a bot.")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "hlp suicide" or message.content == "hlp Suicide":
-        await message.channel.send("Suicide isnt the answer unless youre in IT because then its the right thing to do.")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == "hlp Bruh" or message.content == "hlp bruh":
-        await message.channel.send("Bruh why am I supposed to help you huh?")
-
+    # If the content of the message is a key in the message dictionary
+    if message.content in messages.message_dict:
+        # Then send a message that matches the key
+        await message.channel.send(messages.message_dict[message.content])
+        # Print output to the console
+        print("Sent message in " + message.channel.name + ":" + 
+                messages.message_dict[message.content])
 
 client.run(tokens["discord"])
---
+++ src/messages.py
@@ -0,0 +1,10 @@
+message_dict = {
+    "hlp hi" : "Hello! Thank you for contacting C3 Tech! A tech will be able to assist you shortly!",
+    "hlp remote" : "Hi! Please go to help.myc3.tech and a tech will give you were code when you are at the screen that says 'please join with a code!'",
+    "hlp pam" : "PIss off",
+    "hlp ur mum" : "My Mom? I think you mean your mom cause I did her last night and im a bot.",
+    "hlp your mom" : "My Mom? I think you mean your mom cause I did her last night and im a bot.",
+    "ur mom" : "My Mom? I think you mean your mom cause I did her last night and im a bot.",
+    "hlp suicide" : "Suicide isnt the answer unless youre in IT because then its the right thing to do.",
+    "hlp bruh" : "Bruh why am I supposed to help you huh?"
+}
